ddc6ac5c379ec65ecfb326389c84d829fc9f08b7
Guoker3 <245266575@qq.com>
2021-12-23 15:07:02 +0800

    wash the data roughly,waiting to write plot scripts or so-on,to pick and relect better,this work may be done in program d_apyori

--- GuanWang.py
+++ GuanWang.py
@@ -1,9 +1,23 @@
 import HtmlSpyder as hs
 import HtmlReader as hr
+import time
+import csv
 def CookGuanWang():
-    dataSet=list()
+    controlFlag=list()
+    controlFlag.append("print")
+    controlFlag.append("save")
     ul=hs.openUrl("GuanWang")
+    datasetName="GuanWangRaw2_firstTry.csv"
+    f=open(datasetName,"a+")
+    writer=csv.writer(f)
+    featureName=["embeddedDepth","lineNumber","imgWidth","imgHeight","red","green","blue","colorVariety","contrast"]
+    featureName.extend(["levelDistanceLowRatio", "levelDistanceHighRatio", "levelSimiliarDistanceLowRatio", "levelSimiliarDistanceHighRatio"])
+    featureName.extend(['verticalZeroRatio', 'verticalMinusRatio', 'verticalPositiveRatio', 'verticalSimiliarZeroRatio','verticalSimiliarMinusRatio', 'verticalSimiliarPositiveRatio'])
+    featureName.extend(['horizonDistanceCloserRatio', 'horizonDistanceFatherRatio','horizonDistanceInFoundLevelCloserRatio', 'horizonDistanceInFoundLevelFatherRatio'])
+    featureName.extend(['childNumber', 'childTagNumber', 'siblingNumber', 'siblingTagNumber', 'uncleNumber','uncleTagNumber'])
+    writer.writerow(featureName)
     for url in ul:
+        print(time.strftime("Day%d %H:%M:%S",time.localtime()), "\t", url)
         if (url == ""):
             continue
         html=hs.getHtml(url)
@@ -36,7 +50,17 @@ def CookGuanWang():
                 if nearbyAmountFeature==None:
                     continue
                 features.update(nearbyAmountFeature)
-                print(features)
+                if "print" in controlFlag:
+                    print(features)
                 #wash and save to file
+                if "save" in controlFlag:
+                    featureWashed=list()
+                    featureWashed.extend([features['depth'],features['width'],features['shape'][0],features['shape'][1],features['color'][0],features['color'][1],features['color'][2],features['colorKinds'],features['contrast']])
+                    featureWashed.extend([features['levelDistanceLowRatio'], features['levelDistanceHighRatio'], features['levelSimiliarDistanceLowRatio'], features['levelSimiliarDistanceHighRatio'], features['verticalZeroRatio'], features['verticalMinusRatio']])
+                    featureWashed.extend([features['verticalPositiveRatio'], features['verticalSimiliarZeroRatio'], features['verticalSimiliarMinusRatio'], features['verticalSimiliarPositiveRatio']])                    
+                    featureWashed.extend([features['horizonDistanceCloserRatio'],features['horizonDistanceFatherRatio'],features['horizonDistanceInFoundLevelCloserRatio'], features['horizonDistanceInFoundLevelFatherRatio']])
+                    featureWashed.extend([features['childNumber'], features['childTagNumber'],features['siblingNumber'], features['siblingTagNumber'], features['uncleNumber'], features['uncleTagNumber']])
+                    writer.writerow(featureWashed)
+    f.close()
 if __name__=="__main__":
     CookGuanWang()
